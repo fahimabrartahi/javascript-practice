1b2378c115009293a06b7e5c62ed80d0691285ba
fahimabrartahi <fahim.abrar.mail@gmail.com>
2026-08-09 04:06:01 +0600

    Day 1 - JS fundamentals: variable, conditional, loops, functions

---
+++ day1.js
@@ -0,0 +1,17 @@
+function greet(name){
+    return "Hello "+name;
+}
+let result=greet("fahim");
+console.log(result);
+
+function add(a, b){
+    return a+b;
+}
+let addition=add(1,2);
+console.log(addition);
+
+function isEven(x){
+    return x%2==0;
+}
+let evencheck=isEven(2);
+console.log(evencheck);
